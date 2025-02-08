be00331e4a070828b2dbaa9f05ec655dcec762fb
Giuseppe Genito <81335465+giusgenito@users.noreply.github.com>
2025-02-08 10:54:19 +0100

    Add files via upload

---
+++ preprocessed_images_RGB/Misure_di_acc.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import cv2
+import os
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, confusion_matrix, classification_report
+from tabulate import tabulate
+
+x_train = np.load("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Training_Set_NpArr.npy", allow_pickle=True)
+x_valid = np.load("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Validation_Set_NpArr.npy", allow_pickle=True)
+x_test = np.load("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Test_Set_NpArr.npy", allow_pickle=True)
+y_train = np.load("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Training_Set_Y.npy", allow_pickle=True)
+y_valid = np.load("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Validation_Set_Y.npy", allow_pickle=True)
+y_test = np.load("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Test_Set_Y.npy", allow_pickle=True)
+
+# Carica il modello salvato
+model = tf.keras.models.load_model('C:/Users/CASALAB/Desktop/Gruppo_20_Favb/Modelli/tentativo47')
+classes = ["Italian", "English", "German", "Spanish", "Dutch", "Russian", "Japanese", "French"]
+
+y_prediction = model.predict(x_test)
+y_prediction = np.argmax(y_prediction, axis = 1)
+
+#Create confusion matrix
+result = confusion_matrix(y_test, y_prediction )
+
+plt.figure(figsize=(10, 10))
+plt.imshow(result, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title('Matrice di confusione')
+plt.colorbar()
+plt.xticks(np.arange(len(classes)), classes, rotation=45)
+plt.yticks(np.arange(len(classes)), classes)
+plt.xlabel('Etichetta predetta')
+plt.ylabel('Etichetta reale')
+thresh = result.max() / 2.
+for i in range(result.shape[0]):
+    for j in range(result.shape[1]):
+        plt.text(j, i, format(result[i, j], 'd'),
+                 horizontalalignment="center",
+                 color="white" if result[i, j] > thresh else "black")
+
+plt.show()
+
+# Calcola l'accuratezza
+accuracy = accuracy_score(y_test, y_prediction)
+print("Accuratezza: ", accuracy)
+
+#Accuratezza per ogni classe
+def accuracy_per_class(confusion_matrix):
+    num_classes = confusion_matrix.shape[0]
+    accuracies = np.zeros(num_classes)
+
+    for i in range(num_classes):
+        correct_predictions = confusion_matrix[i, i]
+        total_samples = np.sum(confusion_matrix[i, :])
+        accuracies[i] = correct_predictions / total_samples
+
+    return accuracies
+
+class_accuracies = accuracy_per_class(result)
+print(class_accuracies)
+
+# Calcola la precision
+precision = precision_score(y_test, y_prediction, average=None)
+#print("Precisione per classe: ", precision)
+
+# Calcola il recall
+recall = recall_score(y_test, y_prediction, average=None)
+#print("Recupero per classe: ", recall)
+
+# Calcola il punteggio F1
+f1 = f1_score(y_test, y_prediction, average=None)
+#print("Punteggio F1 per classe: ", f1)
+
+# Crea un DataFrame con i valori
+df = pd.DataFrame({'Classe': classes, 'Precision': precision, 'Recall': recall, 'F1-score': f1,'Accuracy': class_accuracies})
+
+# Formatta le colonne del DataFrame
+df['Precision'] = df['Precision'].apply(lambda x: '{:.2f}'.format(x))
+df['Recall'] = df['Recall'].apply(lambda x: '{:.2f}'.format(x))
+df['F1-score'] = df['F1-score'].apply(lambda x: '{:.2f}'.format(x))
+df['Accuracy'] = df['Accuracy'].apply(lambda x: '{:.2f}'.format(x))
+
+# Converte il DataFrame in una tabella formattata
+table = tabulate(df, headers='keys', tablefmt='pretty', showindex=False)
+
+# Stampa la tabella
+print(table)
+
+model.summary()
---
+++ preprocessed_images_RGB/PreProcessing.py
@@ -0,0 +1,198 @@
+import pandas as pd
+import numpy as np
+import cv2
+
+path_train = "C:/Users/CASALAB/Desktop/Gruppo_20_Favb/Dataset/Lips_video_10_seconds_division_train_test_val/Train"
+path_valid = "C:/Users/CASALAB/Desktop/Gruppo_20_Favb/Dataset/Lips_video_10_seconds_division_train_test_val/Validation"
+path_test = "C:/Users/CASALAB/Desktop/Gruppo_20_Favb/Dataset/Lips_video_10_seconds_division_train_test_val/Test"
+
+train_df = pd.read_csv("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/FileCsv/file_train.csv")
+valid_df = pd.read_csv("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/FileCsv/file_val.csv")
+test_df = pd.read_csv("C:/Users/CASALAB/Desktop/Gruppo_20_Favb/FileCsv/file_test.csv")
+
+#------------------------------------------------------------------------------------------------
+
+dir_train = path_train
+dir_valid = path_valid
+dir_test = path_test
+
+def num_less_zero(num):
+  if num == 1:
+    return num-1
+  if num == 2:
+    return num-1
+  if num == 3:
+    return num-1
+  if num == 4:
+    return num-1
+  if num == 5:
+    return num-1
+  if num == 6:
+    return num-1
+  if num == 7:
+    return num-1
+  if num == 8:
+    return num-1
+
+def prepare_train(train_df, dir):
+    num_samples = len(train_df)
+    video_name_paths = train_df["VideoName"].values.tolist()
+    labels = []
+    path_video = []
+    for i in range(num_samples):
+        path_video.append(dir + "/" + video_name_paths[i])
+        labels.append(num_less_zero(int(str(video_name_paths[i])[0])))
+    return path_video, labels
+
+def prepare_validation(validation_df, dir):
+    num_samples = len(validation_df)
+    video_name_paths = validation_df["VideoName"].values.tolist()
+    labels = []
+    path_video = []
+    for i in range(num_samples):
+        path_video.append(dir + "/" + video_name_paths[i])
+        labels.append(num_less_zero(int(str(video_name_paths[i])[0])))
+    return path_video, labels
+
+def prepare_test(test_df, dir):
+    num_samples = len(test_df)
+    video_name_paths = test_df["VideoName"].values.tolist()
+    labels = []
+    path_video = []
+    y_lable = pd.DataFrame(columns = ["Nomi_video", "Etichette"])
+
+    for i in range(num_samples):
+        path_video.append(dir + "/" + video_name_paths[i])
+
+        labels.append(num_less_zero(int(str(video_name_paths[i])[0])))
+        nome_video = video_name_paths[i].replace(".avi", "")
+        nome_video = nome_video.replace("_m", "")
+        y_lable.loc[i] = (nome_video,labels[i])
+    return path_video, labels, y_lable
+
+path_train, labels_train = prepare_train(train_df, dir_train)
+path_valid, labels_valid = prepare_train(valid_df, dir_valid)
+path_test, labels_test, y_lable = prepare_test(test_df, dir_test)
+
+y_lable.to_csv('C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Y_lable_test_con_nomi.csv', index=False)
+
+#------------------------------------------------------------------------------------------------
+
+def pre_processing_train(path_train, labels_train):
+    path_train = path_train
+    labels_train = labels_train
+    array_videos = []
+
+    for i in range(len(path_train)):
+        video_array = []
+        n_frame = 0
+        cap = cv2.VideoCapture(path_train[i])
+        print(int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+
+        while cap.isOpened():
+            ret, image = cap.read()
+            n_frame += 1
+            if not ret or n_frame > 150:
+                break
+
+            image = cv2.resize(image, (50, 50), interpolation=cv2.INTER_AREA)
+            image.shape = (50, 50, 3)
+            video_array.append(image)
+
+        video_array = np.array(video_array)
+        array_videos.append([video_array, labels_train[i]])
+
+    x = []
+    y = []
+    for variabile, lingua in array_videos:
+        x.append(variabile)
+        y.append(lingua)
+
+    x = np.array(x)
+    y = np.array(y)
+
+    np.save('C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Training_Set_NpArr.npy', x)
+    np.save('C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Training_Set_Y.npy', y)
+
+pre_processing_train(path_train, labels_train)
+
+#------------------------------------------------------------------------------------------------
+
+def pre_processing_validation(path_valid, labels_valid):
+    path_valid = path_valid
+    labels_valid = labels_valid
+    array_videos = []
+
+    for i in range(len(path_valid)):
+        video_array = []  ## ogni elemento è un frame di un video
+        n_frame = 0
+        cap = cv2.VideoCapture(path_valid[i])
+
+        while cap.isOpened():
+            ret, image = cap.read()
+            n_frame += 1
+            if not ret or n_frame > 150:
+                break
+
+            image = cv2.resize(image, (50, 50), interpolation=cv2.INTER_AREA)
+            image.shape = (50, 50, 3)
+            video_array.append(image)
+
+        video_array = np.array(video_array)
+        array_videos.append([video_array, labels_valid[i]])
+
+    x = []
+    y = []
+
+    for variabile, lingua in array_videos:
+        x.append(variabile)
+        y.append(lingua)
+
+    x = np.array(x)
+    y = np.array(y)
+
+    np.save('C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Validation_Set_NpArr.npy', x)
+    np.save('C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Validation_Set_Y.npy', y)
+
+pre_processing_validation(path_valid, labels_valid)
+
+#------------------------------------------------------------------------------------------------
+
+def pre_processing_test(path_test, labels_test):
+    path_test = path_test
+    labels_test = labels_test
+    array_videos = []
+
+    for i in range(len(path_test)):
+        video_array = []  #
+        n_frame = 0
+        cap = cv2.VideoCapture(path_test[i])
+
+        while cap.isOpened():
+            ret, image = cap.read()
+            n_frame += 1
+            if not ret or n_frame > 150:
+                break
+
+            image = cv2.resize(image, (50, 50), interpolation=cv2.INTER_AREA)
+            image.shape = (50, 50, 3)
+            video_array.append(image)
+
+        video_array = np.array(video_array)
+        array_videos.append([video_array, labels_test[i]])
+
+    x = []
+    y = []
+    for variabile, lingua in array_videos:
+        x.append(variabile)
+        y.append(lingua)
+
+    x = np.array(x)
+    y = np.array(y)
+
+    np.save('C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Test_Set_NpArr.npy', x)
+    np.save('C:/Users/CASALAB/Desktop/Gruppo_20_Favb/PreProcessing/Test_Set_Y.npy', y)
+
+pre_processing_test(path_test, labels_test)
+
+#------------------------------------------------------------------------------------------------
